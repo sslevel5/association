1d7ea0b1bd62fe1beea2ffc8e3dcd838461e595c
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-06 13:01:03 +0000

    root_20240306

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'tweets/new'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/create'
-  get 'users/index'
-  get 'users/show'
+  root 'tweets#index'
+
+  resources :tweets
+  resources :users
+  # get 'tweets/new'
+  # get 'tweets/index'
+  # get 'tweets/show'
+  # get 'tweets/create'
+  # get 'users/index'
+  # get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

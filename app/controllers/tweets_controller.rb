69e9e7f369ec5ba160842fd30a505fc94ecb97f3
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-08 07:28:29 +0000

    root修正_20240308

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,15 +14,15 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.new(tweet_params)
-    @tweet.user_id = current_user.id
-    @tweer.save
+    @tweet = Tweet.new(tweet_params)  # フォームから送られてきたデータ(body)をストロングパラメータを経由して@tweetに代入
+    @tweet.user_id = current_user.id # user_idの情報はフォームからはきていないので、deviseのメソッドを使って「ログインしている自分のid」を代入
+    @tweet.save
     redirect_to tweets_path
   end
 
   private
     def tweet_params
-      params.require(:tweet).permit(:body)
+      params.require(:tweet).permit(:body) # tweetモデルのカラムのみを許可
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   root 'tweets#index'
 
+  devise_for :users
   resources :tweets
   resources :users
-  # get 'tweets/new'
-  # get 'tweets/index'
-  # get 'tweets/show'
-  # get 'tweets/create'
-  # get 'users/index'
-  # get 'users/show'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
